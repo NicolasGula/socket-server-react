60fdf3b1eb562bdba5388ff271bde664a47bf026
nicolas gula <nicolas.bonzini@cencosud.com>
2022-11-05 20:20:41 -0300

    Primer commit

---
+++ models/sockets.js
@@ -0,0 +1,19 @@
+class Sockets {
+  constructor(io) {
+    this.io = io;
+    this.socketEvents();
+  }
+
+  socketEvents() {
+    //On connection
+    this.io.on("connection", (socket) => {
+      //Escuchar el evento: message-to-server
+      socket.on("message-to-server", (data) => {
+        console.log(data);
+        this.io.emit("message-from-server", data);
+      });
+    });
+  }
+}
+
+module.exports = Sockets;
